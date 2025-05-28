8ea81e4d0bf779c4748746684ac08a941b3cc4af
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2025-05-28 15:48:49 -0300

    Add .gitignore and stop tracking build directory

--- build/CMakeFiles/HellDefender.dir/src/color_palette/color_palette.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/HellDefender.dir/src/color_palette/color_palette.c.o: \
- /home/aluno/Projects/Hell-Defender-RE/src/color_palette/color_palette.c \
- /usr/include/stdc-predef.h \
- /home/aluno/Projects/Hell-Defender-RE/include/color_palette/color_palette.h \
- /home/aluno/Projects/Hell-Defender-RE/libraries/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/stdarg.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/stdbool.h
